19f618458b6c0978dda21f0c7f5fa9a7c1cfbe58
Ilya-PMO4 <ilyapopel2007@gmail.com>
2024-12-28 16:24:32 +0200

    new mode

--- write-words.py
+++ write-words.py
@@ -2,20 +2,20 @@ from alt import *
 
 with open("./words.csv", "a+") as file:
 	start = True
-	print("режим(1_ - із фото. 2_ - без фото), Слово, Значення, Приклад, мітка(verb, noun...), href-google, href-name, href-alt\n")
+	print("режим(1_ - із фото. 2_ - без фото),, Слово,, Значення,, Додатково,, Приклад,, мітка(verb, noun...),, href-google,, href-name,, href-alt\n")
 
 	while start:
 		blank_text = input()
-		blank_words = blank_text.split(', ')
+		blank_words = blank_text.split(',, ')
 
 		if blank_text == 'q':
 			break
 
-		elif blank_words[0] == "1_" and len(blank_words) == 8:
-			file.write(f"{blank_words[1]}|{blank_words[2]}|{blank_words[3]}|{blank_words[5]}|{blank_words[6]}|{blank_words[7]}|{blank_words[4]}\n")
+		elif blank_words[0] == "1_" and len(blank_words) == 9:
+			file.write(f"{blank_words[1]}|{blank_words[2]}|{blank_words[3]}|{blank_words[4]}|{blank_words[6]}|{blank_words[7]}|{blank_words[8]}|{blank_words[5]}\n")
 			
-		elif blank_words[0] == "2_" and len(blank_words) == 5:
-			file.write(f"{blank_words[1]}|{blank_words[2]}|{blank_words[3]}|{href_google}|{href_name}|{href_alt}|{blank_words[4]}\n")
+		elif blank_words[0] == "2_" and len(blank_words) == 6:
+			file.write(f"{blank_words[1]}|{blank_words[2]}|{blank_words[3]}|{blank_words[4]}|{href_google}|{href_name}|{href_alt}|{blank_words[5]}\n")
 			
 		else:
-			print("Рядок має бути коректним, маючи роздільник: ', ' або 'q' для виходу")
+			print("Рядок має бути коректним, маючи роздільник: ',, '\t\t'q' для виходу")
